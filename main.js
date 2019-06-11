68e312e32e2ae793dadca018c91b41c31ea8537e
Takamasa Komiya <t.komiya@opt.ne.jp>
2019-06-11 16:34:12 +0900

    eslint

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const content = document.getElementById("content"); //root content
 
 class OkimochiCharacter {
-    constructor(character){
+    constructor(character) {
         this.character = character;
         this.node = document.createElement("div");
         this.node.textContent = character;
-        this.node.className = "okimochi-char";    
+        this.node.className = "okimochi-char";
     }
 
-    choiceColor(){
+    choiceColor() {
         const colorList = ["red", "lightblue", "lightgreen", "yellow", "white"];
         this.node.style.color = colorList[(Math.floor(Math.random() * 100) % colorList.length)]
     }
 }
 
 class OkimochiContainer {
-    constructor(...args){
+    constructor(...args) {
         this.args = args;
         this.node = document.createElement("div");
         content.appendChild(this.node);
@@ -24,19 +24,19 @@ class OkimochiContainer {
         this.setIntervalHandle = false;
     }
 
-    choiceStyle(){
-        const styleList = ["row","row-reverse","column","column-reverse"];
-        const alignList = ["flex-start","center","flex-end"];
-        const className = ["okimochi",styleList[Math.floor(Math.random() * styleList.length)],alignList[Math.floor(Math.random() * alignList.length)]].join(" ");
+    choiceStyle() {
+        const styleList = ["row", "row-reverse", "column", "column-reverse"];
+        const alignList = ["flex-start", "center", "flex-end"];
+        const className = ["okimochi", styleList[Math.floor(Math.random() * styleList.length)], alignList[Math.floor(Math.random() * alignList.length)]].join(" ");
         this.node.className = className;
         this.args.forEach(e => e.choiceColor());
     }
 
-    okimochiAutoReflesh(){
+    okimochiAutoReflesh() {
         const setIntervalHandle = this.setIntervalHandle;
         const refleshTime = 5000;
         clearInterval(setIntervalHandle);
-        if(!setIntervalHandle) this.setIntervalHandle = setInterval(this.choiceStyle.bind(this), refleshTime);
+        if (!setIntervalHandle) this.setIntervalHandle = setInterval(this.choiceStyle.bind(this), refleshTime);
         else this.setIntervalHandle = false;
     }
 }
@@ -45,8 +45,8 @@ const O = new OkimochiCharacter("お")
 const KI = new OkimochiCharacter("き")
 const MO = new OkimochiCharacter("も")
 const CHI = new OkimochiCharacter("ち")
-const allOKIMOCHI = new OkimochiContainer(O,KI,MO,CHI)
+const allOKIMOCHI = new OkimochiContainer(O, KI, MO, CHI)
 allOKIMOCHI.choiceStyle();
 
-O.node.addEventListener("click",allOKIMOCHI.choiceStyle.bind(allOKIMOCHI),false);
-CHI.node.addEventListener("click",allOKIMOCHI.okimochiAutoReflesh.bind(allOKIMOCHI),false);
+O.node.addEventListener("click", allOKIMOCHI.choiceStyle.bind(allOKIMOCHI), false);
+CHI.node.addEventListener("click", allOKIMOCHI.okimochiAutoReflesh.bind(allOKIMOCHI), false);
